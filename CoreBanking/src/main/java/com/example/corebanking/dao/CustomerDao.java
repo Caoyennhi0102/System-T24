0c2cdf56a46a9126c342273782a79f241ba98501
Caoyennhi0102 <luuvinhphat1487@gmail.com>
2024-10-28 07:58:26 +0700

    hhaha

---
+++ CoreBanking/src/main/java/com/example/corebanking/dao/CustomerDao.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.corebanking.dao;
+
+import com.example.corebanking.config.DatabaseConfig;
+import com.example.corebanking.model.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import java.util.List;
+
+/**
+ *
+ * @author PC
+ */
+public class CustomerDao {
+    private DatabaseConfig databaseConfig = new DatabaseConfig();
+    
+    public void SaveCustomer(C)
+}
